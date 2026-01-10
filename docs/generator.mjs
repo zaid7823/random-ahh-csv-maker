bbbe87cc76c4462ce047a30cf7830d47d1367144
Zaid Patel <119334718+zaid7823@users.noreply.github.com>
2026-01-10 13:28:08 +0530

    Refactor CSV generation with current date function

--- docs/generator.mjs
+++ docs/generator.mjs
@@ -1,15 +1,23 @@
 import { BIOMARKER_VALUES } from "./mappings.mjs";
 
+function returnTime() {
+    const d = new Date();
+    const pad = (n, len = 2) => n.toString().padStart(len, '0');
+
+    return `${d.getFullYear()}-${pad(d.getMonth() + 1)}-${pad(d.getDate())} ` +
+        `${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}.${pad(d.getMilliseconds(), 3)}`;
+}
+
 function renderRow(barcode, biomarker) {
     return [
         barcode,
         barcode,
         barcode,
         7533,
-        '1978-01-01',
-        '2022-11-15 07:09:19.930',
-        '2022-11-16 09:33:19.930',
-        '2022-11-16 10:02:07.930',
+        '2000-11-11',
+        '2026-01-10 09:41:00.930',
+        '2026-01-10 09:41:00.930',
+        returnTime,
         biomarker,
         BIOMARKER_VALUES[biomarker],
         '',
